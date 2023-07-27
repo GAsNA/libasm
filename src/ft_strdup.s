f243f78547f789a216acaae2a463e9c19ff85fd0
GAsNa <romane.leseur.pro@gmail.com>
2023-07-27 11:59:05 +0200

    errno on strdup (malloc)

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -3,6 +3,7 @@ section .text
 	extern	malloc
 	extern	ft_strlen
 	extern	ft_strcpy
+	extern	__errno_location
 
 ft_strdup:
 	call	ft_strlen				;length of rdi in rax
@@ -10,7 +11,17 @@ ft_strdup:
 	mov		rbx,rdi					;keep value rdi
 	mov		rdi,rax					;value to malloc
 	call	malloc					;call malloc, return pointer to rax
+	cmp		rax,0					;compare rax and 0
+	jl		errno					;jump to error
 	mov		rdi,rax					;rdi = rax
 	mov		rsi,rbx					;rsi = rsx
 	call	ft_strcpy				;call ft_strcpy, return value in rax
 	ret								;return
+
+errno:
+	neg		rax						;absolute value
+	mov		rdi,rax					;rdi = rax
+	call	__errno_location		;call to errno location
+	mov		[rax],rdi				;set the value to errno
+	mov		rax,-1					;rax = -1
+	ret
