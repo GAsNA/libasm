1550fcb9e9ce1fe4c92718473cc7d2b9c09f1635
GAsNa <romane.leseur.pro@gmail.com>
2023-07-27 11:52:39 +0200

    errno on read and write

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,7 +1,18 @@
 section .text
-	global ft_read			;begin, entrypoint
+	global	ft_read			;begin, entrypoint
+	extern	__errno_location
 
 ft_read:
 	mov	rax,0
 	syscall
+	cmp		rax,0			;compare rax and 0
+	jl		errno			;jump to error
 	ret						;return
+
+errno:
+	neg		rax					;absolute value
+	mov		rdi,rax				;rdi = rax
+	call	__errno_location	;call to errno location
+	mov		[rax],rdi			;set the value to errno
+	mov		rax,-1				;rax = -1
+	ret
--- src/ft_write.s
+++ src/ft_write.s
@@ -1,7 +1,20 @@
 section .text
 	global ft_write			;begin, entrypoint
+	extern __errno_location
 
 ft_write:
-	mov	rax,1
-	syscall
+	mov		rax,1			;rax = 1, 1 is for sys write
+	syscall					;system call
+	cmp		rax,0			;compare rax and 0
+	jl		errno			;jump to error
 	ret						;return
+
+errno:
+	neg		rax					;absolute value
+	mov		rdi,rax				;rdi = rax
+	call	__errno_location	;call to errno location
+	mov		[rax],rdi			;set the value to errno
+	mov		rax,-1				;rax = -1
+	ret
+
+;https://stackoverflow.com/questions/62046052/how-to-change-errno-value-in-asm-x64
